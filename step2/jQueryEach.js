9a967f905404addb0e1f83d5ca07cd9439ce36d6
wolfieorama <wachirawamunyi@gmail.com>
2016-10-13 13:45:17 +0300

    more advanced each and map

--- step2/jQueryEach.js
+++ step2/jQueryEach.js
@@ -13,3 +13,32 @@ $.map(cities, function(city. index){
   console.log(result);// Paris 0 London 1  Orlando 2 but our new  array result is ['Paris 0', 'London 1', 'Orlando 2'];
   return result
 });
+
+//real example
+
+$('button').on('click', function() {
+  $.ajax('/cities/deals', {
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function(result) {
+      $.each(result, function(index, dealItem) {
+        var dealElement = $('.deal-' + index);
+        dealElement.find('.name').html(dealItem.name);
+        dealElement.find('.price').html(dealItem.price);
+      });
+    }
+  });
+});
+
+
+//can also be cleaned up to
+
+('button').on('click', function() {
+  $.getJSON('/cities/deals', function(result) {
+    $.each(result, function(index, dealItem) {
+      var dealElement = $('.deal-' + index);
+      dealElement.find('.name').html(dealItem.name);
+      dealElement.find('.price').html(dealItem.price);
+    });
+  });
+});
